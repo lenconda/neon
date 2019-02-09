fb2b88a2b71c3a7d5597234492595fda937724a2
lenconda <1053464288@qq.com>
2019-02-09 21:18:36 +0800

    🎉 finish database models

---
+++ src/database/models/insert_item.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const insertItemData = new mongoose.Schema({
+  uuid: { type: String, index: true, required: true },
+  url: { type: String, required: true },
+  filename: { type: String, required: true },
+  content: { type: String, required: true },
+  created_at: { type: String, required: true },
+  updated_at: { type: String, required: true }}, { timestamp: true })
+
+const InsertItemDataModel =
+  mongoose.model('datas', insertItemData)
+
+module.exports = InsertItemDataModel
