901b7c94df86f98c51a4cfd870199d7fdbdedc10
lenconda <1053464288@qq.com>
2019-02-09 21:07:37 +0800

    ✨ add MongoDBConnection module

---
+++ src/database/connection.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME } = require('../config')
+const getLogger = require('../utils/logger')
+
+const logger = getLogger(__filename)
+
+const _uri = Symbol('uri')
+const _database = Symbol('database')
+
+class MongoDBConnection {
+
+  constructor () {
+    let authorize = (DB_USER && DB_PASSWORD) ?
+      `${DB_USER}:${DB_PASSWORD}@` : ''
+    this[_uri] = `mongodb://${authorize}${DB_HOST}:${DB_PORT}/${DB_NAME}`
+    this[_database]
+  }
+
+  connect () {
+    this[_database] =
+      mongoose
+        .connect(this[_uri], { useNewUrlParser: true,
+          useCreateIndex: true })
+        .catch(err => logger.error(err.message))
+  }
+
+  disconnect () {
+    mongoose.disconnect()
+  }
+
+}
+
+module.exports.DBModel = MongoDBConnection
