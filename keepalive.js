742655074bbc85e42f37f19552869267115a5982
lenconda <1053464288@qq.com>
2019-02-15 22:47:21 +0800

    🐛 shorten restart interval

--- keepalive.js
+++ keepalive.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron')
 const { exec } = require('child_process')
 
-cron.schedule('0 15,45 * * * *', () => {
+cron.schedule('0 0,15,30,45 * * * *', () => {
   exec('pm2 restart neon-publisher')
 })
